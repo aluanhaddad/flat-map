5f0e243ac8decea9febd746e9f6bff58638ae3b1
Aluan Haddad <aluanh@gmail.com>
2017-06-20 08:09:49 -0400

    rename */register.js -> */shim.js

--- src/flatten/shim.js
+++ src/flatten/shim.js
@@ -1,4 +1,4 @@
-import flatten from './index';
+import flatten from './index.js';
 
 if (!Object.prototype.hasOwnProperty.call(Array.prototype, 'flatten')) {
   Array.prototype.flatten =/**@memberof {Array}*/ flatten;

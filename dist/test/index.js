a25fda2453fca2f13eb1315af3b5ad6f4464c3ff
Aluan Haddad <aluanh@gmail.com>
2017-06-20 08:08:25 -0400

    fix .gitignore and remove dist

--- dist/test/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var _tape = require('tape');
-
-var _tape2 = _interopRequireDefault(_tape);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }

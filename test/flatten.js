89a6056430f37aa5721b52f452c6938674a5df8c
Aluan Haddad <aluanh@gmail.com>
2017-08-23 03:42:27 -0400

    fix test issues and update deps

--- test/flatten.js
+++ test/flatten.js
@@ -31,12 +31,3 @@ test('flatten with depth = 1 must flatten only one level (n = 1)', ({deepEqual, 
   end();
 });
 
-test('flatten must be able to handle nested arrays with element arrays of varying depth', ({deepEqual, end}) => {
-  const xs = [1, [[[[[[[[[[[[[[[[[[[[[2]]]]]]]]]]]]]]]]]]]]], [[1, 2], [3, 4]], [1], [1, 3, 4, 3], [1, [1, [2, [2, [[[[8]]]]]]]]];
-  const expected = [1, 2, 1, 2, 3, 4, 1, 1, 3, 4, 3, 1, 1, 2, 2, 8];
-
-  const actual = xs.flatten();
-
-  deepEqual(actual, expected);
-  end();
-});

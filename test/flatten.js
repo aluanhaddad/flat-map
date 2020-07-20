23cea3ac2fa569d3b27e2dbd86f9dee8ad269550
Aluan Haddad <aluanh@gmail.com>
2020-07-20 00:09:02 -0400

    Update test

--- test/flatten.js
+++ test/flatten.js
@@ -1,5 +1,5 @@
 import test from 'tape';
-import '../src/flatten';
+import '../src/flat';
 
 test('flatten with depth = 1 must flatten first level nesting (n = 1)', ({deepEqual, end}) => {
   const xs = [[1, 2, 3], [4, 5, 6]];
@@ -11,21 +11,21 @@ test('flatten with depth = 1 must flatten first level nesting (n = 1)', ({deepEq
   end();
 });
 
-test('flatten with depth = 2 must flatten second level nesting (n = 2)', ({deepEqual, end}) => {
+test('flat with depth = 2 must flatten second level nesting (n = 2)', ({deepEqual, end}) => {
   const xs = [[1, 2, 3], [4, 5, [6]]];
   const expected = [1, 2, 3, 4, 5, 6];
 
-  const actual = xs.flatten(2);
+  const actual = xs.flat(2);
 
   deepEqual(actual, expected);
   end();
 });
 
-test('flatten with depth = 1 must flatten only one level (n = 1)', ({deepEqual, end}) => {
+test('flat with depth = 1 must flatten only one level (n = 1)', ({deepEqual, end}) => {
   const xs = [[1, 2, 3], [4, 5, [6]]];
   const expected = [1, 2, 3, 4, 5, [6]];
 
-  const actual = xs.flatten(1);
+  const actual = xs.flat(1);
 
   deepEqual(actual, expected);
   end();
